a4b0d774bfa7218f4d1f1725196e57d9caaae4ab
Jakub Szymański <55634397+bukej@users.noreply.github.com>
2024-06-02 15:28:02 +0200

    Style: added blobs animation on logo hover

--- src/components/Blobs/Blobs.tsx
+++ src/components/Blobs/Blobs.tsx
@@ -1,11 +1,16 @@
-import Blob1 from "../../assets/blobs/blob1.svg?react"
-import Blob2 from "../../assets/blobs/blob2.svg?react"
-import styles from "./Blobs.module.scss"
-const Blobs = () => (
+import Blob1 from "../../assets/blobs/blob1.svg?react";
+import Blob2 from "../../assets/blobs/blob2.svg?react";
+import styles from "./Blobs.module.scss";
+const Blobs = () => {
+  return (
     <div className={styles.container}>
-        <Blob1 style={{left: "-1200px", top: "-1100px"}} className={styles.blob1}/>
-        <Blob2 style={{right: "-800px"}} className={styles.blob2}/>
+      <Blob1
+        style={{ left: "-1200px", top: "-1100px" }}
+        className={`${styles.blob1} blob1`}
+      />
+      <Blob2 style={{ right: "-800px" }} className={`${styles.blob2} blob2`} />
     </div>
-)
+  );
+};
 
-export default Blobs
+export default Blobs;
