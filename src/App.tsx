9df51cd51477077c56b37a0bb0cca721f6459d00
Jakub Szymański <55634397+bukej@users.noreply.github.com>
2024-06-13 22:08:22 +0200

    Disabled projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-/// <reference types="vite-plugin-svgr/client" />
-
-import styles from "./App.module.scss"
+import styles from "./App.module.scss";
 import WelcomePageLoadingBar from "./components/WelcomePageLoadingBar/WelcomePageLoadingBar.tsx";
-import ProjectsSection from "./components/ProjectsSection/ProjectsSection.tsx";
 import Blobs from "./components/Blobs/Blobs.tsx";
 import Navigation from "./components/Navigation/Navigation.tsx";
 
@@ -10,13 +7,13 @@ function App() {
   return (
     <>
       <main className={styles.main}>
-          <Navigation />
-          <ProjectsSection />
-          <WelcomePageLoadingBar />
-          <Blobs />
+        <Navigation />
+        {/*<ProjectsSection />*/}
+        <WelcomePageLoadingBar />
+        <Blobs />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
